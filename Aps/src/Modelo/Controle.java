1eb876967269c7ff4da5cf661555787f218e6da4
Juliostocco <38107777+Juliostocco@users.noreply.github.com>
2018-11-01 21:29:15 -0300

    Add files via upload

---
+++ Aps/src/Modelo/Controle.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author julio
+ */
+public class Controle 
+{
+    public class Ordenacao {
+ 
+    public int[] insertionSort(int[] vetor) {
+        long tempoinicial = System.currentTimeMillis();
+        for (int i = 0; i < vetor.length; i++) {
+            int atual = vetor[i];
+            int j = i - 1;
+            while (j >= 0 && vetor[j] >= atual) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            vetor[j + 1] = atual;;
+        }
+        long tempofinal = System.currentTimeMillis();
+        long tempototal = tempofinal - tempoinicial;
+        System.out.println("Tempo de Processamento de InsertionSort: " + tempototal + "ms");
+        return vetor;
+    }
+ 
+    public int[] bubbleSort(int vetor[]) {
+        long tempoinicial = System.currentTimeMillis();
+        for (int i = vetor.length; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                if (vetor[j - 1] > vetor[j]) {
+                    int aux = vetor[j];
+                    vetor[j] = vetor[j - 1];
+                    vetor[j - 1] = aux;
+                }
+            }
+        }
+        long tempofinal = System.currentTimeMillis();
+        long tempototal = tempofinal - tempoinicial;
+        System.out.println("Tempo de Processamento de BubbleSort: " + tempototal + "ms");
+        return vetor;
+    }
+ 
+    public int[] mergeSort(int[] array) {
+        long tempoinicial = System.currentTimeMillis();
+        array = MergeSort.sort(array);
+        long tempofinal = System.currentTimeMillis();
+        long tempototal = tempofinal - tempoinicial;
+        System.out.println("Tempo de Processamento de MergeSort: " + tempototal + "ms");
+        return array;
+    }
+    
+    public int[] quickSort(int[] array) {
+        long tempoinicial = System.currentTimeMillis();
+        array = QuickSort.quicksort(array, 0, (array.length - 1));
+        long tempofinal = System.currentTimeMillis();
+        long tempototal = tempofinal - tempoinicial;
+        System.out.println("Tempo de Processamento de QuickSort: " + tempototal + "ms");
+        return array;
+    }
+      
+    public void imprimirVetor(int[] array) {
+        for (int counter = 0; counter < (array.length - 1); counter++) {
+            System.out.println(array[counter]);
+        }
+    }
+}
+}
---
+++ Aps/src/Modelo/Ordenacao.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author julio
+ */
+public class Ordenacao 
+{
+     public int[] bubbleSort(int vetor[]) 
+    {
+        long tempoinicial = System.currentTimeMillis();
+        for (int i = vetor.length; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                if (vetor[j - 1] > vetor[j]) {
+                    int aux = vetor[j];
+                    vetor[j] = vetor[j - 1];
+                    vetor[j - 1] = aux;
+                }
+            }
+        }
+        long tempofinal = System.currentTimeMillis();
+        long tempototal = tempofinal - tempoinicial;
+        System.out.println("Tempo de Processamento de BubbleSort: " + tempototal + "ms");
+        return vetor;
+    }
+    
+    
+    public int[] insertionSort(int[] vetor) 
+    {
+        long tempoinicial = System.currentTimeMillis();
+        for (int i = 0; i < vetor.length; i++) {
+            int atual = vetor[i];
+            int j = i - 1;
+            while (j >= 0 && vetor[j] >= atual) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            vetor[j + 1] = atual;;
+        }
+        long tempofinal = System.currentTimeMillis();
+        long tempototal = tempofinal - tempoinicial;
+        System.out.println("Tempo de Processamento de InsertionSort: " + tempototal + "ms");
+        return vetor;
+    }
+    
+    public class MergeSort {
+ 
+    public static int[] sort(int[] array) {
+       
+        if (array.length <= 1) {
+           
+            return array;
+        }
+        int meio = array.length / 2;
+        int[] dir = array.length % 2 == 0 ? new int[meio] : new int[meio + 1];
+        int[] esq = new int[meio];
+ 
+        int[] aux = new int[array.length];
+ 
+        for (int i = 0; i < meio; i++) {
+            esq[i] = array[i];
+        }
+ 
+        int auxIndex = 0;
+        for (int i = meio; i < array.length; i++) {
+            dir[auxIndex] = array[i];
+            auxIndex++;
+        }
+ 
+        esq = sort(esq);
+        dir = sort(dir);
+ 
+        aux = mergesort(esq, dir);
+ 
+        return aux;
+    }
+ }
+ 
+    static int[] mergesort(int[] esq, int[] dir) 
+    {
+        int[] aux = new int[esq.length + dir.length];
+ 
+        int indexDir = 0, indexEsq = 0, indexAux = 0;
+ 
+        while (indexEsq < esq.length || indexDir < dir.length) {
+            if (indexEsq < esq.length && indexDir < dir.length) {
+                if (esq[indexEsq] <= dir[indexDir]) {
+                    aux[indexAux] = esq[indexEsq];
+                    indexAux++;
+                    indexEsq++;
+                } else {
+                    aux[indexAux] = dir[indexDir];
+                    indexAux++;
+                    indexDir++;
+                }
+            } else if (indexEsq < esq.length) {
+                aux[indexAux] = esq[indexEsq];
+                indexAux++;
+                indexEsq++;
+            } else if (indexDir < dir.length) {
+                aux[indexAux] = dir[indexDir];
+                indexAux++;
+                indexDir++;
+            }
+        }
+        return aux;
+    }
+    
+    public class QuickSort {
+ 
+ 
+    public static int[] quicksort(int vet[], int ini, int fim) {
+ 
+        int meio;
+ 
+        if (ini < fim) {
+ 
+            meio = partition(vet, ini, fim);
+ 
+            quicksort(vet, ini, meio);
+ 
+            quicksort(vet, meio + 1, fim);
+ 
+        }
+        
+        return vet;
+ 
+    }
+ 
+ 
+ 
+    public static int partition(int vet[], int ini, int fim) {
+ 
+        int pivo, topo, i;
+ 
+        pivo = vet[ini];
+ 
+        topo = ini;
+ 
+ 
+ 
+        for (i = ini + 1; i <= fim; i++) {
+ 
+            if (vet[i] < pivo) {
+ 
+                vet[topo] = vet[i];
+ 
+                vet[i] = vet[topo + 1];
+ 
+                topo++;
+ 
+            }
+ 
+        }
+ 
+        vet[topo] = pivo;
+ 
+        return topo;
+ 
+    }
+  }
+}
